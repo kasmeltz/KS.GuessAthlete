f13351f897609b053a7186bcdbf9e6363a5b1f48
kasmeltz <kasmeltz@lakeheadu.ca>
2016-03-27 19:52:07 -0500

    Dont want to lose work

---
+++ KS.GuessAthlete.Data/POCO/Athlete.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KS.GuessAthlete.Data.POCO
+{
+    public class Athlete
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<AggregateStat> Stats { get; set; }
+    }
+}
--- KS.GuessAthlete.Data/POCO/Player.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KS.GuessAthlete.Data.POCO
-{
-    public class Player
-    {
-        public string Name { get; set; }
-    }
-}
