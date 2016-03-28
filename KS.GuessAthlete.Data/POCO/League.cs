2db9fdc75dd929c0b01d0e6090cd7669f65a5a43
kasmeltz <kasmeltz@lakeheadu.ca>
2016-03-27 20:32:55 -0500

    WIP

---
+++ KS.GuessAthlete.Data/POCO/League.cs
@@ -0,0 +1,7 @@
+﻿namespace KS.GuessAthlete.Data.POCO
+{
+    public class League : PocoDataObject
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ KS.GuessAthlete.Data/POCO/PocoDataObject.cs
@@ -0,0 +1,7 @@
+﻿namespace KS.GuessAthlete.Data.POCO
+{
+    public class PocoDataObject
+    {
+        public int Id { get; set; }
+    }
+}
