57359b5358c25eecfe8200d57a6f72cb510157b9
kasmeltz <kasmeltz@lakeheadu.ca>
2016-03-29 18:37:35 -0500

    DB WIP

--- KS.GuessAthlete.Database/Scripts/Versions/Version1.sql
+++ KS.GuessAthlete.Database/Scripts/Versions/Version1.sql
@@ -14,21 +14,214 @@ BEGIN
 	SELECT @NHLLeagueId = SCOPE_IDENTITY();
 END;
 
+SELECT 
+	@NHLLeagueId = Id 
+FROM 
+	[app].[League] 
+WHERE 
+	Name = 'National Hockey League';
+
 /* Seasons */
 IF NOT EXISTS (SELECT Id FROM [app].[Season] WHERE Name = '1905-1906 NHL Regular Season')
 BEGIN
 	DECLARE @cnt INT = 1905;
-
-	WHILE @cnt < cnt_total
+	WHILE @cnt < 2030
 	BEGIN
 		INSERT INTO 
 		[app].[Season]
 		(LeagueId, Name, StartDate, EndDate, IsPlayoffs)
 		VALUES
-		(@NHLLeagueId, @cnt + '-' + (@cnt + 1) + ' NHL Regular Season', '1905-09-01', '1906-04-10', 0)
+		(@NHLLeagueId, 
+			CONVERT(varchar(4), @cnt) + '-' + CONVERT(varchar(4), (@cnt + 1)) + ' NHL Regular Season', 
+			CONVERT(varchar(4), @cnt) + '-09-01', 
+			CONVERT(varchar(4), (@cnt + 1)) + '-04-10', 
+			0)
+
+		INSERT INTO 
+		[app].[Season]
+		(LeagueId, Name, StartDate, EndDate, IsPlayoffs)
+		VALUES
+		(@NHLLeagueId, 
+		CONVERT(varchar(4), @cnt) + '-' + CONVERT(varchar(4), (@cnt + 1)) + ' NHL Playoffs', 
+		CONVERT(varchar(4), (@cnt + 1)) + '-04-14', 
+		CONVERT(varchar(4), (@cnt + 1)) + '-06-02', 
+		1)
 
 	   SET @cnt = @cnt + 1;
 	END;
+END;
+
+/* Awards */
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'All Star Voting')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'All Star Voting', 'AS', '1930-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Hart Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Hart Memorial Trophy', 'Hart', '1923-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Lady Byng Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Lady Byng Memorial Trophy', 'Byng', '1924-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Vezina Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Vezina Trophy', 'Vezina', '1926-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Calder Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Calder Memorial Trophy', 'Calder', '1936-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Art Ross Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Art Ross Trophy', 'Ross', '1947-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'James Norris Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'James Norris Memorial Trophy', 'Norris', '1953-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Conn Smythe Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Conn Smythe', 'Smythe', '1964-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Bill Masterton Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Bill Masterton Memorial Trophy', 'Masterton', '1967-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Ted Lindsay Award')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Ted Lindsay Award', 'Lindsay', '1970-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Frank J. Selke Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Frank J. Selke Trophy', 'Selke', '1977-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'William M. Jennings Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'William M. Jennings Trophy', 'Jennings', '1981-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'King Clancy Memorial Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'King Clancy Memorial Trophy', 'Clancy', '1987-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Maurice Richard Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Maurice Richard Trophy', 'Richard', '1998-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Jack Adams Award')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Jack Adams Award', 'Adams', '1973-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Stanley Cup')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Stanley Cup', 'Stanley', '1917-01-01', null);
+END;
+
+IF NOT EXISTS (SELECT Id FROM [app].[Award] WHERE Name = 'Presidents Trophy')
+BEGIN	
+	INSERT INTO 
+		[app].[Award]
+		(LeagueId, Name, Abbreviation, StartDate, EndDate)
+	VALUES
+		(@NHLLeagueId, 'Presidents Trophy', 'Presidents', '1917-01-01', null);
+END;
+
+/* Teams */
+DECLARE @TeamId INT;
+IF NOT EXISTS (SELECT Id FROM [app].[Team] WHERE Name = '')
+BEGIN
+	INSERT INTO 
+		[app].[Team]
+		(LeagueId, Name)
+	VALUES
+		(@NHLLeagueId, '');
+
+	SELECT @TeamId = SCOPE_IDENTITY();
+
+	INSERT INTO 
+		[app].[TeamIdentity]
+		(TeamId, Name, Abbreviation, City, StartDate, EndDate)
+	VALUES
+		(@TeamId, 'National Hockey League');
 END;
 
 /* Initial Users */
