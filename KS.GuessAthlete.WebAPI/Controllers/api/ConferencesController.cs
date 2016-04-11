38fbdcb05ad5b851496a576f36c56a5f5448e06b
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-11 18:28:29 -0500

    WIP WEBAPI

---
+++ KS.GuessAthlete.WebAPI/Controllers/api/ConferencesController.cs
@@ -0,0 +1,31 @@
+﻿using KS.GuessAthlete.Data.DataAccess.Repository.Interface;
+using KS.GuessAthlete.Data.POCO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KS.GuessAthlete.WebAPI.Controllers.api
+{
+    public class ConferencesController : BaseApiController
+    {
+        public ConferencesController() : base () { }
+
+        // GET: api/conferences
+        public async Task<HttpResponseMessage> Get()
+        {
+            try
+            {
+                IConferenceRepository repository =
+                    RepositoryCollection.Conferences();
+                IEnumerable<Conference> conferences = await repository.List();
+                return Request.CreateResponse(HttpStatusCode.OK, conferences);
+            }
+            catch (Exception ex)
+            {
+                return CreateResponseError("CONFERENCE_LIST_ERROR", ex);
+            }
+        }        
+    }
+}
---
+++ KS.GuessAthlete.WebAPI/Controllers/api/DivisionsController.cs
@@ -0,0 +1,31 @@
+﻿using KS.GuessAthlete.Data.DataAccess.Repository.Interface;
+using KS.GuessAthlete.Data.POCO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KS.GuessAthlete.WebAPI.Controllers.api
+{
+    public class DivisionsController : BaseApiController
+    {
+        public DivisionsController() : base () { }
+
+        // GET: api/divisions
+        public async Task<HttpResponseMessage> Get()
+        {
+            try
+            {
+                IDivisionRepository repository =
+                    RepositoryCollection.Divisions();
+                IEnumerable<Division> divisions = await repository.List();
+                return Request.CreateResponse(HttpStatusCode.OK, divisions);
+            }
+            catch (Exception ex)
+            {
+                return CreateResponseError("DIVISION_LIST_ERROR", ex);
+            }
+        }        
+    }
+}
