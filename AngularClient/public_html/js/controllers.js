8a215d8946645b886813731527e8c6b2c0d91b25
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-12 14:13:41 -0500

    WIP client side guessing game

--- AngularClient/public_html/js/controllers.js
+++ AngularClient/public_html/js/controllers.js
@@ -14,10 +14,32 @@ app.controller('homeController', ['$scope', '$route', '$pickAthleteDataService',
 		};
 		
         $pickAthleteDataService.pickAthlete(options, function (data) {
-            alert(data);
-			alert(data.Name);
+			$scope.athlete = data;
         });
     };
-
+	
+	$scope.askQuestion = function(question) {
+		var yesNo = 'no';
+		
+		var names = $scope.athlete.Name.split(' ');		
+		if (question.selectedName == 'first') {
+			var name = names[0];
+			if (name.substring(0,1) == question.selectedLetter) {
+				yesNo = 'yes';
+			} 
+		} else if (question.selectedName == 'last') {
+			var name = names[names.length-1];
+			if (name.substring(0,1) == question.selectedLetter) {
+				yesNo = 'yes';
+			} 
+		}
+		
+		question.answered = yesNo;		
+		$scope.questionsAsked.push({selectedLetter:'A',selectedName:'first'});
+	}
+	
+	$scope.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];	
+	$scope.names = ['first', 'last'];	
+	$scope.questionsAsked = [{selectedLetter:'A',selectedName:'first'}];
     $scope.getRandomAthlete();
 }]);
