514a2f9e620b1040f3ffa23a2cbd8b1f88a10c3b
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-12 11:08:43 -0500

    hockey athlete picker class

--- KS.GuessAthlete.Logic/AthletePicker/Hockey/HockeyAthletePicker.cs
+++ KS.GuessAthlete.Logic/AthletePicker/Hockey/HockeyAthletePicker.cs
@@ -1,9 +1,58 @@
-﻿namespace KS.GuessAthlete.Logic.AthletePicker.Hockey
+﻿using KS.GuessAthlete.Data.DataAccess.Repository.Interface;
+using KS.GuessAthlete.Data.POCO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace KS.GuessAthlete.Logic.AthletePicker.Hockey
 {
     /// <summary>
     /// Picks a random athlete based on some criteria
     /// </summary>
     public class HockeyAthletePicker
     {
+        protected IRepositoryCollection Repository { get; set; }
+
+        public HockeyAthletePicker(IRepositoryCollection repository)
+        {
+            Repository = repository;
+        }
+
+        public async Task<Athlete> PickAthlete(int skaterGamesPlayed, int skaterPoints,
+            decimal skaterPPG, int goalieGamesPlayed, int goalieWins, int startYear)
+        {
+            IAthleteRepository athleteRepository = Repository.Athletes();
+            IDraftRepository draftRepository = Repository.Drafts();
+            IJerseyNumberRepository jerseyNumberRepository = Repository.JerseyNumbers();
+            IAthleteAwardRepository athleteAwardRepository = Repository.AthleteAwards();
+            ISkaterStatLineRepository skaterStatLineRepository = Repository.SkaterStatLines();
+            IGoalieStatLineRepository goalieStatLineRepository = Repository.GoalieStatLines();
+
+            List<int> athleteIds = (await athleteRepository
+                .SkatersForCriteria(skaterGamesPlayed, skaterPoints, skaterPPG, startYear))
+                .ToList();
+            athleteIds.AddRange(await athleteRepository
+                .GoaliesForCriteria(goalieGamesPlayed, goalieWins, startYear));
+
+            Random rnd = new Random();
+            int chosenId = athleteIds[rnd.Next(0, athleteIds.Count)];
+
+            Athlete athlete = await athleteRepository.Get(chosenId);
+            athlete.Drafts = await draftRepository.ForAthlete(chosenId);
+            athlete.JerseyNumbers = await jerseyNumberRepository.ForAthlete(chosenId);
+            athlete.Awards = await athleteAwardRepository.ForAthlete(chosenId);
+
+            if (athlete.Position.ToUpper() == "G")
+            {
+                athlete.Stats = await goalieStatLineRepository.ForAthlete(chosenId);
+            }
+            else
+            {
+                athlete.Stats = await skaterStatLineRepository.ForAthlete(chosenId);
+            }
+
+            return athlete;
+        }
     }
 }
