bde73931e939a92d9b5a3a1a44720bb035ebf0d5
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-12 18:16:54 -0500

    Adding images and new controllers

---
+++ KS.GuessAthlete.WebAPI/Controllers/api/TeamIdentityDivisionsController.cs
@@ -0,0 +1,31 @@
+﻿using KS.GuessAthlete.Data.DataAccess.Repository.Interface;
+using KS.GuessAthlete.Data.POCO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KS.GuessAthlete.WebAPI.Controllers.api
+{
+    public class TeamIdentityDivisionsController : BaseApiController
+    {
+        public TeamIdentityDivisionsController() : base() { }
+
+        // GET: api/teamidentitydivisions
+        public async Task<HttpResponseMessage> Get()
+        {
+            try
+            {
+                ITeamIdentityDivisionRepository repository =
+                    RepositoryCollection.TeamIdentityDivisions();
+                IEnumerable<TeamIdentityDivision> teamIdentityDivisions = await repository.List();
+                return Request.CreateResponse(HttpStatusCode.OK, teamIdentityDivisions);
+            }
+            catch (Exception ex)
+            {
+                return CreateResponseError("TEAM_IDENTITY_DIVISION_LIST", ex);
+            }
+        }
+    }
+}
