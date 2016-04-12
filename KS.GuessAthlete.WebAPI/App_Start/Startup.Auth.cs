192ee5dfa0cd1d0e57847e89eee7aa369bbc00b0
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-12 12:47:49 -0500

    Added CORS ability to WebApi

--- KS.GuessAthlete.WebAPI/App_Start/Startup.Auth.cs
+++ KS.GuessAthlete.WebAPI/App_Start/Startup.Auth.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using KS.GuessAthlete.WebAPI.Models;
+using KS.GuessAthlete.WebAPI.Providers;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using KS.GuessAthlete.WebAPI.Providers;
-using KS.GuessAthlete.WebAPI.Models;
+using System;
 
 namespace KS.GuessAthlete.WebAPI
 {
@@ -22,6 +19,8 @@ namespace KS.GuessAthlete.WebAPI
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            app.UseCors(CorsOptions.AllowAll);
+
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
